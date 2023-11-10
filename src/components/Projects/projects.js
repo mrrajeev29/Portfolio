1a36bd00c87a9442139ef6a8bcb5a4f3e2d046f1
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2023-11-10 14:35:02 +0530

    Add files via upload

---
+++ src/components/Projects/projects.js
@@ -0,0 +1,59 @@
+import React from "react";
+import './projects.css'
+const Projects=()=>{
+    return (
+        <section id="project">
+            <h1 className="title">Some of my Projects.</h1>
+            <div className="pro1">
+                <h4 className="proTit">Vpropel Clone</h4>
+                <h4 className="proDesc">A Coding platform for students used by VIT.</h4>
+                <a href="https://github.com/mrrajeev29/Vpropel-clone" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://mrrajeev29.github.io/Vpropel-clone/" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Open</button></a>
+            </div>
+            <div className="pro2">
+                <h4 className="proTit">Portfolio</h4>
+                <h4 className="proDesc">This project is a Portfolio website for my friend and created using MERN.</h4>
+                <a href="https://github.com/mrrajeev29/Portfolio-project" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://portfolio-project-lac-five.vercel.app/" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Open</button></a>
+            </div>
+            <div className="pro1">
+                <h4 className="proTit">Calculator Using React </h4>
+                <h4 className="proDesc">A basic Arithmetic Calculator using MERN.</h4>
+                <a href="https://github.com/mrrajeev29/Calculator-Using-React" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://calculator-using-react-psi.vercel.app/" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Open</button></a>
+            </div>
+            <div className="pro2">
+                <h4 className="proTit">Elephant Image Classification</h4>
+                <h4 className="proDesc">Classification of elephants using Deep Learning</h4>
+                <a href="https://github.com/mrrajeev29/Elephant_Image_Classification" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://github.com/mrrajeev29/Elephant_Image_Classification/blob/main/Elephant_Image_Classification.ipynb" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Explore</button></a>
+            </div>
+            <div className="pro1">
+                <h4 className="proTit">Transcription of video files using API.</h4>
+                <h4 className="proDesc">This project can produce transcription of video files.</h4>
+                <a href="https://github.com/mrrajeev29/Transcription-of-video-file-using-API" target="_blank"><button target="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://github.com/mrrajeev29/Transcription-of-video-file-using-API/blob/main/Rajeev_Transcript.ipynb" target="_blank"><button target="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Explore</button></a>
+            </div>
+            <div className="pro2">
+                <h4 className="proTit">Speech to text using Generative Ai.</h4>
+                <h4 className="proDesc">Using Generative Ai to convert speech to text.</h4>
+                <a href="https://github.com/mrrajeev29/Speech_to_text_Generative_Ai_using_openAi_apikey" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://github.com/mrrajeev29/Speech_to_text_Generative_Ai_using_openAi_apikey/blob/main/Rajeev_Ranjan_Text_to_Text_Ai.ipynb" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Explore</button></a>
+            </div>
+            <div className="pro1">
+                <h4 className="proTit">Heart Disease Prediction.</h4>
+                <h4 className="proDesc">Using R Programming language this model is trained for predicting heart disease and also include Statistical Analysis.</h4>
+                <a href="https://github.com/mrrajeev29/Heart-Disease-Prediction" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://github.com/mrrajeev29/Heart-Disease-Prediction/blob/main/Heart%20Disease%20Prediction.pptx" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Explore</button></a>
+            </div>
+            <div className="pro2">
+                <h4 className="proTit">Covid Data Visualization</h4>
+                <h4 className="proDesc">Visualization of Covid 19 happened all over the world.</h4>
+                <a href="https://github.com/mrrajeev29/Covid_Data_Visualization" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-github"></i>&ensp;Learn More</button></a>
+                <a href="https://github.com/mrrajeev29/Covid_Data_Visualization/blob/main/Worldometer_data_Visualization.ipynb" target="_blank"><button type="button" className="Pbtn"><i class="fa fa-external-link">&ensp;</i>Explore</button></a>
+            </div>
+            <a href="https://github.com/mrrajeev29" target="_blank"><button type="button" className="Abtn"><i class="fa fa-github"></i>&ensp;All</button></a>
+        </section>
+    )
+}
+export default Projects
