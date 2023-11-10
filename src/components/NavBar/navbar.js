804cb79c59177047962f3073d18286dd7a6e6b7d
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2023-11-10 14:34:16 +0530

    Update navbar.js

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -15,6 +15,7 @@ const Navbar=()=>
                 <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
                 <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">About</Link>
                 <Link activeClass="active" to='works' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Portfolio</Link>
+                <Link activeClass="active" to='project' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Projects</Link>
                 <Link activeClass="active" to='clients' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Clients</Link>
             </div>
             <button className="desktopMenuBtn" onClick={()=>{
@@ -28,8 +29,9 @@ const Navbar=()=>
                 <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>Home</Link>
                 <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>About</Link>
                 <Link activeClass="active" to='works' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>Portfolio</Link>
+                <Link activeClass="active" to='project' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>Projects</Link>
                 <Link activeClass="active" to='clients' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>Clients</Link>
-                <Link activeClass="active" to='clients' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>Contact</Link>
+                <Link activeClass="active" to='contactPage' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(!false)}>Contact</Link>
             </div>
 
 
