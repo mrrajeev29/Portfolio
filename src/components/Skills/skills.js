dc27d2146e6320ce2926818b61447b607f1f034a
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-04-22 00:14:36 +0530

    Update skills.js

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -138,7 +138,7 @@ const Skills=()=>{
     return(
         <section id="skills">
             <span className="skillTitle">What I do</span>
-            <span className="skillDesc">I am a skilled web designer with experience in creating visual appealing and user-friendl<br/>websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML,CSS and JavaScript, as well as I am good in Data Science and Analysis.</span>
+            <span className="skillDesc">I am Computer Science Student and a skilled web designer with experience in creating visual appealing and user-friendly<br/>websites. I have a strong understanding of design and a keen eye for detail. I am proficient in JavaScript and in MERN development as well as I am good in Data Science and Analysis.</span>
             <div className="skillBars">
                 <div className="skillBar">
                     <img src={UIDesign} alt="UIDesign" className="skillBarImg"/>
